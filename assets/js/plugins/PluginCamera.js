babdea6a8e40f553ffe17cbc71e66fbb185861b3
Gallo <felippe.gallo@gmail.com>
2016-07-04 23:56:40 -0300

    Commit Seg Jul  4 23:56:40 BRT 2016

--- assets/js/plugins/PluginCamera.js
+++ assets/js/plugins/PluginCamera.js
@@ -1,4 +1,8 @@
-function camera(type){
+function PluginCamera() {
+	this.cache = "";
+}
+PluginCamera.prototype.takePicture = function(type) {
+	var self = this;
 	if(type=="album"){
 		navigator.camera.getPicture(success, error,{quality: 75,targetWidth:800,targetHeight:600,destinationType: Camera.DestinationType.FILE_URI,sourceType: Camera.PictureSourceType.SAVEDPHOTOALBUM});
 	}else{
@@ -6,9 +10,10 @@ function camera(type){
 	}
 	function success(image) {
 		console.log(image);
+		self.cache = image;
 		return(JSON.stringify(image));
 	}
 	function error(message) {
 		console.log('Failed because: ' + message);
 	}
-}
+};
--- main.js
+++ main.js
@@ -1,34 +1,6 @@
-/**
+/*
  * Copyright 2016 Felippe Gallo All Rights Reserved.
  */
-
-//ONDEVICEREADY
-/*document.addEventListener("deviceready", onDeviceReady, false);
-function onLoad(){
-	if(!window.cordova){
-		setTimeout(function(){
-			onDeviceReady();
-		},1000);
-	}
-}
-function onDeviceReady(){
-	function camera(type){
-		if(type=="album"){
-			navigator.camera.getPicture(success, error,{quality: 75,targetWidth:800,targetHeight:600,destinationType: Camera.DestinationType.FILE_URI,sourceType: Camera.PictureSourceType.SAVEDPHOTOALBUM});
-		}else{
-			navigator.camera.getPicture(success, error,{quality: 75,targetWidth:800,targetHeight:600,destinationType: Camera.DestinationType.FILE_URI,sourceType: Camera.PictureSourceType.CAMERA});
-		}
-		function success(image) {
-			console.log(image);
-			return(JSON.stringify(image));
-		}
-		function error(message) {
-			console.log('Failed because: ' + message);
-		}
-	}
-	console.log("ok");
-}*/
-
 
 var myApp = new Framework7();
 var $$ = Dom7;
@@ -47,4 +19,14 @@ window.onload = function() {
 	window.Help = new Help();
 	window.Job = new Job();
 	window.JobPic = new JobPic();
+
+	document.addEventListener("deviceready", onDeviceReady, false);
+	if(!window.cordova){
+		setTimeout(function(){
+			onDeviceReady();
+		},1000);
+	}
+	function onDeviceReady(){
+		window.PluginCamera = new PluginCamera();
+	}
 }
