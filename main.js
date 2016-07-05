58e00d8ad8b905828f566a4e0f5e357420cdacfd
Gallo <felippe.gallo@gmail.com>
2016-07-05 00:28:46 -0300

    Commit Ter Jul  5 00:28:46 BRT 2016

--- main.js
+++ main.js
@@ -27,10 +27,8 @@ window.onload = function() {
 		},1000);
 	}
 	function onDeviceReady(){
-		setTimeout(function(){
-			console.log("ok");
-			window.PluginCamera = new PluginCamera();
-			console.log(navigator.camera);
-		},5000);
+		console.log("ok");
+		window.PluginCamera = new PluginCamera();
+		console.log(navigator.camera);
 	}
 }
