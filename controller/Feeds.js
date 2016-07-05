708bc5428e7a9bc53a425a6fbc902d97feabc567
Gallo <felippe.gallo@gmail.com>
2016-07-05 00:52:59 -0300

    Commit Ter Jul  5 00:52:59 BRT 2016

--- controller/Feeds.js
+++ controller/Feeds.js
@@ -3,12 +3,15 @@ App.prototype.FeedsScreen = function() {
 	var self = this;
 	var Feeds = window.Feed.read();
 	Feeds.then(function(result) {
+		resultRev = [];
 		for(var i in result){
 			result[i].created = moment(result[i].created).format('DD/MM/YY - HH:mm');
+			resultRev.push(result[i])
 		}
+		resultRev.reverse();
 		$.get("templates/Feeds.html", function(temp) {
 			var compiledTemplate = Template7.compile(temp);
-			var html = compiledTemplate(result);
+			var html = compiledTemplate(resultRev);
 			div.html(html);
 		});
 	});		
