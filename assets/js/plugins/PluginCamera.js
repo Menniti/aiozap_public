25aa92f3384bf79638c6d0822bd9bf6b4e54f5fc
Gallo <felippe.gallo@gmail.com>
2016-07-11 08:34:44 -0300

    Commit Seg Jul 11 08:34:44 BRT 2016

--- assets/js/plugins/PluginCamera.js
+++ assets/js/plugins/PluginCamera.js
@@ -5,11 +5,11 @@ PluginCamera.prototype.takePicture = function(type) {
 	var self = this;
 	var deferred = $.Deferred();
 	if(type=="album"){
-		//navigator.camera.getPicture(success, error,{quality: 75,targetWidth:800,targetHeight:600,destinationType: Camera.DestinationType.FILE_URI,sourceType: Camera.PictureSourceType.SAVEDPHOTOALBUM});
-		navigator.camera.getPicture(success, error,{quality: 75,targetWidth:800,targetHeight:600,destinationType: Camera.DestinationType.DATA_URL,sourceType: Camera.PictureSourceType.SAVEDPHOTOALBUM});
+		navigator.camera.getPicture(success, error,{quality: 75,targetWidth:800,targetHeight:600,destinationType: Camera.DestinationType.FILE_URI,sourceType: Camera.PictureSourceType.SAVEDPHOTOALBUM});
+		//navigator.camera.getPicture(success, error,{quality: 75,targetWidth:800,targetHeight:600,destinationType: Camera.DestinationType.DATA_URL,sourceType: Camera.PictureSourceType.SAVEDPHOTOALBUM});
 	}else{
-		//navigator.camera.getPicture(success, error,{quality: 75,targetWidth:800,targetHeight:600,destinationType: Camera.DestinationType.FILE_URI,sourceType: Camera.PictureSourceType.CAMERA});
-		navigator.camera.getPicture(success, error,{quality: 75,targetWidth:800,targetHeight:600,destinationType: Camera.DestinationType.DATA_URL,sourceType: Camera.PictureSourceType.CAMERA});
+		navigator.camera.getPicture(success, error,{quality: 75,targetWidth:800,targetHeight:600,destinationType: Camera.DestinationType.FILE_URI,sourceType: Camera.PictureSourceType.CAMERA});
+		//navigator.camera.getPicture(success, error,{quality: 75,targetWidth:800,targetHeight:600,destinationType: Camera.DestinationType.DATA_URL,sourceType: Camera.PictureSourceType.CAMERA});
 	}
 	function success(image) {
 		//console.log(image);
