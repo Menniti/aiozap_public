a9c1bebfc06f8ceede7c7be5c050b1f2c8cc28b9
Gallo <felippe.gallo@gmail.com>
2016-07-12 08:42:30 -0300

    Commit Ter Jul 12 08:42:30 BRT 2016

--- controller/TakeJobPic.js
+++ controller/TakeJobPic.js
@@ -10,60 +10,10 @@ App.prototype.TakeJobPicAction = function() {
 		for(var i in result){
 			var button = {};
 			button.text = result[i].title;
-			button.onClick = function(){
-				var JobId = i;
-				//TAKE PICTURE
-				var JobPicture = window.PluginCamera.takePicture();
-				JobPicture.then(function(resultPic) {
-					var pic = resultPic;
-					window.JobPic.title = result[i].title+" "+moment(new Date().getTime()).format('DD/MM/YY - HH:mm');
-					window.JobPic.user = window.App.auth.currentUser.uid;
-					window.JobPic.job = JobId;
-					window.JobPic.active = 1;
-					console.log(JobId);
-					//CREATE ENTRY ON DB
-					var JobPics = window.JobPic.create();
-					JobPics.then(function(resultCreate) {
-						if(resultCreate){
-							console.log(pic);
-							var file = pic;
-							window.JobPic.id = resultCreate;
-							window.JobPic.file = file;
-							//UPLOAD PICTURE
-							var JobPics = window.JobPic.uploadFile();
-							$.when(JobPics).then(function(result) {
-								var uploadTask = result;
-								uploadTask.on("state_changed",
-									function(snapshot) {
-										var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-										myApp.addNotification({hold:1000,title: 'Upload:',message:progress+'%'});
-									}, function(error) {
-										console.log(error);
-									}, function() {
-										var downloadURL = uploadTask.snapshot.downloadURL;
-										window.JobPic.file = downloadURL;
-										//UPDATE CREATED ENTRY ON DB
-										var JobPics = window.JobPic.updateFile();
-										JobPics.then(function(result) {
-											if(result==true){
-												myApp.alert(self.msgSuccessDefault,self.msgDefaultTitle);
-											}else{
-												myApp.alert(self.msgErrorDefault,self.msgDefaultTitle);
-											}
-										});		
-
-									}
-								);
-							});
-
-
-						}else{
-							myApp.alert(self.msgErrorDefault,self.msgDefaultTitle);
-						}
-					});		
-				});
-			};
+			button.i = i;
+			button.onClick = self.TakeJobPicClosure;
 			buttons.push(button);
+			console.log(buttons);
 		}
 		var button = {};
 			button.text = "Cancelar";
@@ -72,5 +22,60 @@ App.prototype.TakeJobPicAction = function() {
 		myApp.actions(this,buttons);
 	});		
 	
+};
+
+App.prototype.TakeJobPicClosure = function() {
+	var self = window.App;
+	//window.JobPic.title = result.title+" "+moment(new Date().getTime()).format('DD/MM/YY - HH:mm');
+	window.JobPic.title = this.text+" "+moment(new Date().getTime()).format('DD/MM/YY - HH:mm');
+	window.JobPic.user = window.App.auth.currentUser.uid;
+	window.JobPic.job = this.i;
+	window.JobPic.active = 1;
+	console.log(window.JobPic);
+	//TAKE PICTURE
+	var JobPicture = window.PluginCamera.takePicture();
+	JobPicture.then(function(resultPic) {
+		var pic = resultPic;
+		//CREATE ENTRY ON DB
+		var JobPics = window.JobPic.create();
+		JobPics.then(function(resultCreate) {
+			if(resultCreate){
+				console.log(pic);
+				var file = pic;
+				window.JobPic.id = resultCreate;
+				window.JobPic.file = file;
+				//UPLOAD PICTURE
+				var JobPics = window.JobPic.uploadFile();
+				$.when(JobPics).then(function(result) {
+					var uploadTask = result;
+					uploadTask.on("state_changed",
+						function(snapshot) {
+							var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+							myApp.addNotification({hold:1000,title: 'Upload:',message:progress+'%'});
+						}, function(error) {
+							console.log(error);
+						}, function() {
+							var downloadURL = uploadTask.snapshot.downloadURL;
+							window.JobPic.file = downloadURL;
+							//UPDATE CREATED ENTRY ON DB
+							var JobPics = window.JobPic.updateFile();
+							JobPics.then(function(result) {
+								if(result==true){
+									myApp.alert(self.msgSuccessDefault,self.msgDefaultTitle);
+								}else{
+									myApp.alert(self.msgErrorDefault,self.msgDefaultTitle);
+								}
+							});		
+
+						}
+					);
+				});
+
+
+			}else{
+				myApp.alert(self.msgErrorDefault,self.msgDefaultTitle);
+			}
+		});		
+	});
 };
 
