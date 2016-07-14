526c179cbc752ac31ef8876259aba62f89522676
gallomac <felippe.gallo@gmail.com>
2016-07-14 18:38:48 -0300

    Commit Qui Jul 14 18:38:47 BRT 2016

--- controller/Login.js
+++ controller/Login.js
@@ -8,11 +8,13 @@ App.prototype.LoginScreen = function() {
 
 App.prototype.LoginAction = function(e) {
 	var self = this;
+	$("#btn_login").attr("disabled",true);
 	e.preventDefault();
 	window.User.email = $("#input_email").val();
 	window.User.password = $("#input_password").val();
 	var Users = window.User.login();
 	Users.then(function(result) {
+		$("#btn_login").attr("disabled",false);
 		console.log(result);
 		if(result==true){
 			mainView.back();
@@ -24,11 +26,13 @@ App.prototype.LoginAction = function(e) {
 
 App.prototype.RegisterAction = function(e) {
 	var self = this;
+	$("#btn_register").attr("disabled",true);
 	e.preventDefault();
 	window.User.email = $("#input_register_email").val();
 	window.User.password = $("#input_register_password").val();
 	var Users = window.User.create();
 	Users.then(function(result) {
+		$("#btn_register").attr("disabled",false);
 		console.log(result);
 		if(result==true){
 			mainView.back();
