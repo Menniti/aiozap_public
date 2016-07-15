ba070023b22f13491003bf04238a35aa201847d9
Gallo <felippe.gallo@gmail.com>
2016-07-15 13:46:41 -0300

    Commit Sex Jul 15 13:46:41 BRT 2016

--- controller/Contact.js
+++ controller/Contact.js
@@ -3,21 +3,6 @@ App.prototype.ContactScreen = function() {
 	var div = $("#ContactScreen");
 	var divAdmin = $("#ContactScreenAdmin");
 
-	var Users = window.User.read();
-	Users.then(function(result) {
-		var coords=[];
-		for (var i in result){
-			if(result[i].is_editor==1){
-				coords.push(result[i]);
-			}
-		}
-		$.get("templates/Contact.html", function(temp) {
-			var compiledTemplate = Template7.compile(temp);
-			var html = compiledTemplate(coords);
-			div.html(html);
-		});
-	});		
-
 	var Admins = window.Admin.read();
 	Admins.then(function(result) {
 		var coords=[];
@@ -32,5 +17,49 @@ App.prototype.ContactScreen = function() {
 			divAdmin.html(html);
 		});
 	});		
+
+
+	var Teams = window.Team.read();
+	Teams.then(function(result) {
+		console.log(result);
+
+		var Users = window.User.read();
+		Users.then(function(resultUser) {
+			var coords=[];
+			for (var i in result){
+				for (var j in resultUser){
+					if(resultUser[j].is_editor==1 && resultUser[j].team==i){
+						resultUser[j].team_title = result[i].title;
+						resultUser[j].team_color = result[i].color;
+						coords.push(resultUser[j]);
+					}
+				}
+			}
+			console.log(coords);
+			$.get("templates/Contact.html", function(temp) {
+				var compiledTemplate = Template7.compile(temp);
+				var html = compiledTemplate(coords);
+				div.html(html);
+			});
+		});		
+
+
+	});		
+
+
+	/*var Users = window.User.read();
+	Users.then(function(result) {
+		var coords=[];
+		for (var i in result){
+			if(result[i].is_editor==1){
+				coords.push(result[i]);
+			}
+		}
+		$.get("templates/Contact.html", function(temp) {
+			var compiledTemplate = Template7.compile(temp);
+			var html = compiledTemplate(coords);
+			div.html(html);
+		});
+	});*/		
 
 };
--- controller/Reports.js
+++ controller/Reports.js
@@ -1,43 +1,129 @@
 App.prototype.ReportsScreen = function() {
 	var div = $("#ReportsScreen");
 	var self = this;
-	var Reports = window.Report.read();
-	Reports.then(function(result) {
 
-		resultRev = [];
-		for(var i in result){
-			if(result[i].user == App.auth.currentUser.uid){
-				result[i].id = i;
-				result[i].created = moment(result[i].created).format('DD/MM/YY - HH:mm');
-				resultRev.push(result[i]);
-			}else if(result[i].user == 11){
-				//Caso colaborador, mostra seus relatorios e de sua equipe
+	var Users = window.User.read();
+	Users.then(function(resultUser) {
+		var Reports = window.Report.read();
+		Reports.then(function(result) {
+			resultRev = {};
+			resultMyself = [];
+			resultTeam = [];
+			for(var i in result){
+				if(result[i].user == window.App.auth.currentUser.uid){
+					result[i].id = i;
+					result[i].created = moment(result[i].created).format('DD/MM/YY - HH:mm');
+					resultMyself.push(result[i]);
+				}
+
+				if(resultUser[window.App.auth.currentUser.uid].is_editor==1 && result[i].user != window.App.auth.currentUser.uid && resultUser[window.App.auth.currentUser.uid].team==resultUser[result[i].user].team){
+					result[i].id = i;
+					result[i].created = moment(result[i].created).format('DD/MM/YY - HH:mm');
+					resultTeam.push(result[i]);
+				}
 			}
-		}
-		resultRev.reverse();
-		$.get("templates/Reports.html", function(temp) {
-			var compiledTemplate = Template7.compile(temp);
-			var html = compiledTemplate(resultRev);
-			div.html(html);
-		});
+			resultMyself.reverse();
+			resultTeam.reverse();
+
+			resultRev.resultMyself = resultMyself;
+			resultRev.resultTeam = resultTeam;
+
+			$.get("templates/Reports.html", function(temp) {
+				var compiledTemplate = Template7.compile(temp);
+				var html = compiledTemplate(resultRev);
+				div.html(html);
+			});
+		});		
 	});		
+
 };
 
-App.prototype.ReportsDetailAction = function() {
+App.prototype.ReportsDetailScreen = function() {
 	var self = this;
 	var id = mainView.url.split("id=")[1];
 
 	var div = $("#ReportsDetail");
-	var Reports = window.Report.read(id);
-	Reports.then(function(result) {
-		console.log(result);
-		$.get("templates/ReportsDetail.html", function(temp) {
-			var compiledTemplate = Template7.compile(temp);
-			var html = compiledTemplate(result);
-			div.html(html);
-		});
+
+	var Users = window.User.read();
+	Users.then(function(resultUser) {
+		var Reports = window.Report.read(id);
+		Reports.then(function(result) {
+			result.locked = 1;
+			result.can_validate = 0;
+		
+			if(result.active==0){
+				result.locked = 0;
+			}
+			if(resultUser[window.App.auth.currentUser.uid].is_editor==1){
+				result.locked = 0;
+				result.can_validate = 1;
+			}
+
+
+			$.get("templates/ReportsDetail.html", function(temp) {
+
+				var compiledTemplate = Template7.compile(temp);
+				var html = compiledTemplate(result);
+				div.html(html);
+
+				var ReportDetailForm = $("#report-detail-form");
+				ReportDetailForm.on('submit', self.ReportsDetailAction.bind(self));
+
+				var Jobs = window.Job.read();
+				Jobs.then(function(resultChild) {
+					var div = $("#input_job");
+					$.get("templates/ReportsEditJobSelect.html", function(temp) {
+						var compiledTemplate = Template7.compile(temp);
+						var html = compiledTemplate(resultChild);
+						div.html(html);
+						div.val(result.job);
+					});
+				});		
+
+
+				var Users = window.User.read();
+				Users.then(function(resultChild) {
+					var resultMyTeamUsers = [];
+					for(var i in resultChild){
+						if(resultChild[window.App.auth.currentUser.uid].team==resultChild[i].team){
+							resultChild[i].id = i;
+							resultMyTeamUsers.push(resultChild[i]);
+						}
+					}
+
+					var div = $("#input_user");
+					$.get("templates/ReportsEditUserSelect.html", function(temp) {
+						var compiledTemplate = Template7.compile(temp);
+						var html = compiledTemplate(resultMyTeamUsers);
+						div.html(html);
+						div.val(result.user);
+					});
+				});		
+			});
+		});		
 	});		
 
+
+};
+
+App.prototype.ReportsDetailAction = function(e) {
+	e.preventDefault();
+	var self = this;
+	window.Report.id = mainView.url.split("id=")[1];
+	window.Report.title = $("#input_job option:selected").text()+" - "+$("#input_user option:selected").text()+" - "+moment(new Date().getTime()).format('DD/MM/YY - HH:mm');
+	window.Report.description = $("#input_description").val();
+	window.Report.points = $("#input_points").val();
+	window.Report.active = $("#input_active").val();
+	window.Report.job = $("#input_job").val();
+	window.Report.user = $("#input_user").val();
+	var Reports = window.Report.update();
+	Reports.then(function(result) {
+		if(result==true){
+			myApp.alert(self.msgSuccessDefault,self.msgDefaultTitle);
+		}else{
+			myApp.alert(self.msgErrorDefault,self.msgDefaultTitle);
+		}
+	});		
 };
 
 App.prototype.ReportsAddScreen = function() {
@@ -55,7 +141,6 @@ App.prototype.ReportsAddScreen = function() {
 			div.html(html);
 		});
 	});		
-
 };
 
 App.prototype.ReportsAddAction = function(e) {
@@ -66,7 +151,7 @@ App.prototype.ReportsAddAction = function(e) {
 	window.Report.points = $("#input_points").val();
 	window.Report.job = $("#input_job").val();
 	window.Report.active = 0;
-	window.Report.user = App.auth.currentUser.uid;
+	window.Report.user = window.App.auth.currentUser.uid;
 
 	//TAKE PICTURE
 	var ReportPicture = window.PluginCamera.takePicture();
