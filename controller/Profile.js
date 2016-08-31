f1315202ef46d83f1bd8db5bf5d5a45c70a41c2a
Gallo <felippe.gallo@gmail.com>
2016-08-30 23:47:20 -0300

    Commit Ter Ago 30 23:47:20 BRT 2016

--- controller/Profile.js
+++ controller/Profile.js
@@ -4,11 +4,30 @@ App.prototype.ProfileScreen = function() {
 	var id = mainView.url.split("id=")[1];
 	var Users = window.User.read(id);
 	Users.done(function(result) {
-		console.log(result);
 		$.get("templates/Profile.html", function(temp) {
 			var compiledTemplate = Template7.compile(temp);
 			var html = compiledTemplate(result);
 			div.html(html);
+
+			div = $("#ProfilePicsScreen");
+			var JobPics = window.JobPic.read();
+			JobPics.done(function(result) {
+				var userPics = [];
+				for(var i in result){
+					if(result[i].user==id){
+						result[i].id = i;
+						userPics.push(result[i]);
+					}
+				}
+				console.log(userPics);
+				//userPics.reverse();
+				$.get("templates/ProfilePics.html", function(temp) {
+					var compiledTemplate = Template7.compile(temp);
+					var html = compiledTemplate(userPics);
+					div.html(html);
+				});
+			});		
+
 		});
 	});		
 
