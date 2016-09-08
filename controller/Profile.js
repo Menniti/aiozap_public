b200b5c13738a5fe9fafa2c02d3e99f028f1ed2d
Gallo <felippe.gallo@gmail.com>
2016-09-08 18:08:28 -0300

    Commit Qui Set  8 18:08:28 BRT 2016

--- controller/Profile.js
+++ controller/Profile.js
@@ -5,6 +5,10 @@ App.prototype.ProfileScreen = function() {
 	var Users = window.User.read(id);
 	Users.done(function(result) {
 		$.get("templates/Profile.html", function(temp) {
+
+			result.likes = 0;
+			result.pics = 0;
+
 			var compiledTemplate = Template7.compile(temp);
 			var html = compiledTemplate(result);
 			div.html(html);
@@ -23,14 +27,28 @@ App.prototype.ProfileScreen = function() {
 						userPics.push(result[i]);
 					}
 				}
-				console.log(userPics);
 				//userPics.reverse();
 				$.get("templates/ProfilePics.html", function(temp) {
 					var compiledTemplate = Template7.compile(temp);
 					var html = compiledTemplate(userPics);
 					div.html(html);
+					$("#profilePicsNumber").html(userPics.length);
 				});
+
+				var Likes = window.Like.read();
+				Likes.done(function(result) {
+					var userLikes = 0;
+					for(var i in result){
+						var test = userPics.map(function(e) { return e.id; }).indexOf(result[i].jobpic);
+						if(test>-1){
+							userLikes++;
+						}
+					}
+					$("#profileLikesNumber").html(userLikes);
+				});		
+
 			});		
+
 
 		});
 	});		
